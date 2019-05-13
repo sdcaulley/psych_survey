2683d14aac18e990116be03da05d13754d783675
sdcaulley <stephanie.caulley@gmail.com>
2019-05-13 20:21:04 +0200

    codes for survey displaying on screen

--- scripts/codes.js
+++ scripts/codes.js
@@ -2,9 +2,8 @@ function generate_codes() {
   let seedNum = parseInt(Math.random()*100000000);
   let codeA = window.btoa('a' + seedNum);
   let codeB = window.btoa(seedNum + 'b')
-  document.write('<br>seed = ' + seedNum +' <br>');
-  document.write('codeA = ' + codeA+' <br>');
-  document.write('codeB = ' + codeB+' <br>');
-  document.write('decoded A = ' + window.atob(codeA)+' <br>');
-  document.write('decoded B = ' + window.atob(codeB)+' <br>');
+  $('#your-code').text(codeA);
+  $('#partner-code').text(codeB);
 }
+
+$(document).ready(generate_codes);
