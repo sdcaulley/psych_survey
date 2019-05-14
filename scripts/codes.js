5b41eeed80b7eeaeb0c60ce689716178a1a05659
sdcaulley <stephanie.caulley@gmail.com>
2019-05-14 19:32:08 +0200

    partner url copying to clipboard

--- scripts/codes.js
+++ scripts/codes.js
@@ -2,22 +2,29 @@ function generate_codes() {
   let seedNum = parseInt(Math.random()*100000000);
   let codeA = window.btoa('a' + seedNum);
   let codeB = window.btoa(seedNum + 'b')
-  let urlA = 'https://survey.qualtrics.com/jfe/form/SID=SV_1234?usercode=' + codeA;
-  let urlB = '"https://survey.qualtrics.com/jfe/form/SID=SV_1234?usercode=' + codeB + '"';
   $('#your-code').text(codeA);
   $('#partner-code').text(codeB);
-  $('#your-button').click(function(){
-    window.open(urlA);
-  });
-  $('#partner-button').click(copyText(urlB));
+  yourEvent(codeA);
+  partnerEvent(codeB);
 }
 
-function copyText(element) {
-  let temp = $('<input>');
-  $('body').append(temp);
-  temp.val(element);
-  document.execCommand('copy');
-  temp.remove();
+function yourEvent(code) {
+  let urlA = 'https://survey.qualtrics.com/jfe/form/SID=SV_1234?usercode=' + code;
+  $('#your-button').click(function() {
+    window.open(urlA);
+  });
+}
+
+function partnerEvent(code) {
+  let urlB = 'https://survey.qualtrics.com/jfe/form/SID=SV_1234?usercode=' + code;
+  $('#partner-button').click(function() {
+    let temp = $('<input type="text" id="urlB">').val(urlB);
+    $('#partner-code').append(temp);
+    let text = document.getElementById('urlB');
+    text.select();
+    document.execCommand('Copy');
+    temp.remove();
+  });
 }
 
 $(document).ready(generate_codes);
